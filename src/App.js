c5e5be874210bb4db1554bbd5fa0acd181001461
yhddx <286867796@qq.com>
2019-05-04 19:06:47 +0800

    基础环境

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './index.less';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>Hello React!yhddx</div>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
+    output: {
+        path: path.resolve(__dirname, './dist'), // 输出的路径
+        filename: 'bundle.js'  // 打包后文件
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+}
